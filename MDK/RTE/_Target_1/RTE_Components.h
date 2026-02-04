d7431315b255e8ae22548626766d6fa3f397002a
wangbohan <wangbohan808@163.com>
2026-02-04 10:27:22 +0800

    修改keil相关的跟踪文件

---
+++ MDK/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'H2_WBH' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "cw32l010.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
